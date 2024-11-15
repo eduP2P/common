b65cdd949b56f2c065b68e735ce45f2a7c88ed10
Henk <hb130502@outlook.com>
2024-11-15 16:31:07 +0100

    Turn on IP forwarding in network namespace when creating that namespace to fix failing tests on machines where ip forwarding is off by default

--- test_suite/nat_simulation/create_namespace.sh
+++ test_suite/nat_simulation/create_namespace.sh
@@ -15,3 +15,6 @@ ip netns list | grep -q $name && ip netns del $name
 
 # Add the namespace
 ip netns add $name
+
+# Turn on IP forwarding in the namespace to allow for routing between namespaces
+ip netns exec $name sysctl -w net.ipv4.ip_forward=1
